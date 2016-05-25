f051e7c95b36afc5f595402db5358d3421abc1c7
Andrew Baumann <Andrew.Baumann@microsoft.com>
2016-05-25 16:53:45 -0700

    added a quick and dirty script to copy a file (e.g. the module) into a raspbian image

---
+++ tools/cptovhd.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# exit on failure
+set -ex
+
+if [[ $# != 3 ]]; then
+    echo "Usage: $0 file image dest"
+    exit 1
+fi
+
+# figure out the sector offest of the ext2 partition (partition #2)
+SECTOR=$(partx -g -r -n 2 -o START $2)
+
+TMP=$(mktemp)
+
+# copy out the raw ext2 fs (assuming it runs to the end of the image)
+dd if=$2 of=$TMP bs=512 skip=$SECTOR
+
+# copy the file itself
+e2cp -v $1 $TMP:$3
+
+# copy the ext2 fs back into the image at the same offset
+dd if=$TMP of=$2 bs=512 seek=$SECTOR
+
+# clean up after ourselves
+rm $TMP
