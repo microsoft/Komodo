43a5fdf9d50d7e4fabf6839e8ea3bcd918fffb1b
Andrew Baumann <Andrew.Baumann@microsoft.com>
2016-06-15 21:52:14 -0700

    tweak path

--- guestimg/subdir.mk
+++ guestimg/subdir.mk
@@ -1,5 +1,5 @@
 MODULE_SRC := driver/kevlar.ko
-MODULE_DST := /lib/modules/4.1.18-v7+/kevlar.ko
+MODULE_DST := /lib/modules/kevlar.ko
 
 $(dir)/guestdisk.img: $(MODULE_SRC)
 	[ -f $(dir)/guestdisk.img ] || cp $(GUEST_DISKIMG) $(dir)/guestdisk.img
