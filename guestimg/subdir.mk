f40544ce5bd56e46cd1ea2e7e37ac412f6b1ec29
Andrew Baumann <Andrew.Baumann@microsoft.com>
2016-06-20 14:29:10 -0700

    fix lazy expansion buglet in guestimg subdir.mk
    This subdir stuff is clearly not as composable as it first seemed... :(

--- guestimg/subdir.mk
+++ guestimg/subdir.mk
@@ -1,8 +1,9 @@
 MODULE_SRC := driver/kevlar.ko
 MODULE_DST := /lib/modules/kevlar.ko
+CPTOVHD := $(dir)/cptovhd.sh
 
 $(dir)/guestdisk.img: $(MODULE_SRC)
-	[ -f $(dir)/guestdisk.img ] || cp $(GUEST_DISKIMG) $(dir)/guestdisk.img
-	$(dir)/cptovhd.sh $(MODULE_SRC) $(dir)/guestdisk.img $(MODULE_DST)
+	[ -f $@ ] || cp $(GUEST_DISKIMG) $@
+	$(CPTOVHD) $< $@ $(MODULE_DST)
 
 CLEAN := $(CLEAN) $(dir)/guestdisk.img
