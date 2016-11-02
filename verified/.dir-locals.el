4da1ce8da4cd03d3105507a4f1a6985dacc82aa3
Andrew Baumann <Andrew.Baumann@microsoft.com>
2016-11-02 11:35:15 -0700

    add /ironDafny for consistency with the makefile

--- verified/.dir-locals.el
+++ verified/.dir-locals.el
@@ -2,4 +2,4 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((dafny-mode
-  (dafny-prover-local-args "/noNLarith" "/timeLimit:10")))
+  (dafny-prover-local-args "/ironDafny" "/noNLarith" "/timeLimit:10")))
